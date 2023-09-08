0d33225c8b863cdf69df3a3a5d96b0386e44e8d3
Khushikothiyal11 <101283073+Khushikothiyal11@users.noreply.github.com>
2023-09-08 21:10:32 +0530

    Task 2 :- Student Grade Calculator

---
+++ StudentGradeCalculator.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class StudentGradeCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of subjects: ");
+        int numSubjects = scanner.nextInt();
+        
+        int totalMarks = 0;
+        for (int i = 1; i <= numSubjects; i++) {
+            System.out.print("Enter marks obtained in subject " + i + ": ");
+            int marks = scanner.nextInt();
+            totalMarks += marks;
+        }
+        
+        double averagePercentage = (double) totalMarks / numSubjects;
+        
+        String grade = calculateGrade(averagePercentage);
+
+        System.out.println("\nResults:");
+        System.out.println("Total Marks: " + totalMarks);
+        System.out.println("Average Percentage: " + averagePercentage + "%");
+        System.out.println("Grade: " + grade);
+        
+        scanner.close();
+    }
+    
+    public static String calculateGrade(double percentage) {
+        if (percentage >= 90) {
+            return "A";
+        } else if (percentage >= 80) {
+            return "B";
+        } else if (percentage >= 70) {
+            return "C";
+        } else if (percentage >= 60) {
+            return "D";
+        } else {
+            return "F";
+        }
+    }
+}
